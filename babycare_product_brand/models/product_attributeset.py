b90a0c4d290bfc0dbd9b3b96c7c790a9648fa15c
babycarenl <wjriedstra@gmail.com>
2017-07-25 08:40:01 +0200

    [8.0][babycare_product_brand] import api and import Warning as userError removed because unused

--- babycare_product_brand/models/product_attributeset.py
+++ babycare_product_brand/models/product_attributeset.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api
-from openerp.exceptions import Warning as UserError
+from openerp import models, fields
 
 
 class ProductAttributeSet(models.Model):
