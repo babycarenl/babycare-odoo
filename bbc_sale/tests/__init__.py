d2ba06a5fe253bbff7b9e6326f45b7f5a96ee2c5
Wytze Jan Riedstra <wjriedstra@gmail.com>
2019-06-14 13:18:48 +0200

    [FIX] #88, unpublish EOL product if stock is zero

--- bbc_sale/tests/__init__.py
+++ bbc_sale/tests/__init__.py
@@ -1,5 +1,6 @@
 from . import test_configurable
 from . import test_consu_single_attr
+from . import test_eol_unpublish
 from . import test_supplier_invoice_number
 from . import test_fiscal_position
 from . import test_unique_codes
