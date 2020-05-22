6a6df6a0a9abf171e6392dc73cd7df6aacacfcd5
Wytze Jan Riedstra <wjriedstra@gmail.com>
2020-05-22 12:55:02 +0200

    refactor and filename includes time of export

--- babycare_postnl_export/controllers/main.py
+++ babycare_postnl_export/controllers/main.py
@@ -1,13 +1,20 @@
 # coding: utf-8
-import base64
 from openerp import http
 from openerp.http import request
+
+from datetime import datetime
+from pytz import timezone
+
+import base64
 
 
 class StockPickingController(http.Controller):
     @http.route('/csv/download/stock_picking/<string:csv>', auth='user')
     def sale_order_lines_csv_download(self, csv, **kw):
-        filename = 'export_postnl.csv'
+        amsterdam_timezone = timezone('Europe/Amsterdam')
+        time_now = datetime.now(amsterdam_timezone)
+        filename = 'export_postnl_%s.csv' % time_now.strftime(
+            '%d_%m_%Y_%H_%M_%S')
         csv_output = base64.decodestring(csv)
         return request.make_response(csv_output, [('Content-Type', 'application/octet-stream'),
-                                           ('Content-Disposition', 'attachment; filename="%s"' % filename)])
+                                                  ('Content-Disposition', 'attachment; filename="%s"' % filename)])
