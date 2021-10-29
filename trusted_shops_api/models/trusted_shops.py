c6f114e5dd5fc681eec31025661ee38246c4de1c
Wytze Jan Riedstra <wjriedstra@gmail.com>
2021-10-29 10:12:32 +0200

    trustedshops model

---
+++ trusted_shops_api/models/trusted_shops.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+from openerp import api, fields, models
+
+class TrustedShops(models.Model):
+    _name = 'trusted.shops'
+    trusted_shops_id = fields.Char()
+    language = fields.Char()
