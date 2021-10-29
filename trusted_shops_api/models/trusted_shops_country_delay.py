006de3c87cd5379ab357398d4030042dbe5b11c2
Wytze Jan Riedstra <wjriedstra@gmail.com>
2021-10-29 14:39:39 +0200

    Add required parameter to country_id

--- trusted_shops_api/models/trusted_shops_country_delay.py
+++ trusted_shops_api/models/trusted_shops_country_delay.py
@@ -5,5 +5,5 @@ from openerp import fields, models
 class TrustedShopsCountryDelay(models.Model):
     _name = 'trusted.shops.country.delay'
 
-    country_id = fields.Many2one('res.country', 'Country')
+    country_id = fields.Many2one('res.country', 'Country', required=True)
     delay = fields.Integer(string='Delay', required=True)
