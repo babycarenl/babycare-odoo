778f7c668f80d570d6a2aaaff84055b8795977bf
Wytze Jan Riedstra <wjriedstra@gmail.com>
2021-10-29 19:24:36 +0200

    Add security rules

--- trusted_shops_api/__openerp__.py
+++ trusted_shops_api/__openerp__.py
@@ -10,7 +10,8 @@
     ],
     "data": [
         'views/trusted_shops_api_view.xml',
-        'views/stock_picking.xml'
+        'views/stock_picking.xml',
+        'security/ir.model.access.csv',
     ],
     'license': 'AGPL-3',
 }
